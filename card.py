eb3eeca7d6f29b096dfdfed078b16c69ae031294
Thomas Minh Nguyen <thomasnguyen8503@gmail.com>
2025-04-26 10:05:33 +1200

    New Project started

---
+++ card.py
@@ -0,0 +1,9 @@
+
+class Card:
+    rank = ["Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"]
+    suit = ["Heart", "Diamond", "Spade", "Club"]
+
+    def __init__(self, rank, suit):
+        self.rank = rank
+        self.suit = suit
+
---
+++ readMe.txt
@@ -0,0 +1,4 @@
+
+Finding the best possible combination from 7 different cards from a deck of cards in poker.
+
+In Poker there are
